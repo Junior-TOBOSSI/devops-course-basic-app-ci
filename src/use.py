6b904da6a7eba345e3de76b8a5b89a5440dfa841
gilbertTOBOSSI <gjunadeo@gmail.com>
2025-11-14 16:35:13 +0100

    corrections

--- src/use.py
+++ src/use.py
@@ -2,4 +2,5 @@ import sys
 import json
 from awsip import find_ip, download_aws_ip_set
 
-sys.stdout.write(json.dumps(find_ip(download_aws_ip_set(), sys.argv[1]), indent=2))
+sys.stdout.write(json.dumps(find_ip(download_aws_ip_set(), sys.argv[1]),
+                            indent=2))
